3d9ef3b76df59f329a89ace3852f3e27a92b46d5
Mehdi Abaakouk <sileht@sileht.net>
2016-02-18 10:35:07 +0100

    Bump version to 1.0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils import ccompiler
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-VERSION="1.0.6"
+VERSION="1.0.7"
 
 
 def generate_pyx(recent=None):
