17df451cc6ffee69ede4254fe3a4b6f8d7e29ce9
Mehdi Abaakouk <sileht@sileht.net>
2016-03-25 23:49:28 +0100

    build: cleanup ceph version detection

--- setup.py
+++ setup.py
@@ -14,31 +14,34 @@ from distutils import ccompiler
 
 
 @contextlib.contextmanager
-def stdchannel_redirected(stdchannel, dest_filename):
-    try:
-        oldstdchannel = os.dup(stdchannel.fileno())
-        dest_file = open(dest_filename, 'w')
-        os.dup2(dest_file.fileno(), stdchannel.fileno())
-
-        yield
-    finally:
-        if oldstdchannel is not None:
-            os.dup2(oldstdchannel, stdchannel.fileno())
-        if dest_file is not None:
-            dest_file.close()
+def output_redirected():
+    oldstderr = os.dup(sys.stderr.fileno())
+    oldstdout = os.dup(sys.stdout.fileno())
+    null = open(os.devnull, 'w')
+    os.dup2(null.fileno(), sys.stdout.fileno())
+    os.dup2(null.fileno(), sys.stderr.fileno())
+    yield
+    os.dup2(oldstderr, sys.stderr.fileno())
+    os.dup2(oldstdout, sys.stdout.fileno())
+    null.close()
 
 
 def pre_build_ext(cmd_obj, recent=None):
     # Check librados2 requirements
     if recent is None:
         comp = ccompiler.new_compiler(force=True, verbose=True)
-        rados_installed = comp.has_function('rados_connect',
-                                            libraries=['rados'])
+        with output_redirected():
+            rados_installed = comp.has_function('rados_connect',
+                                                libraries=['rados'])
         if not rados_installed:
             raise Exception("librados2 or librados-dev >= 0.80 are missing")
-        with stdchannel_redirected(sys.stderr, os.devnull):
+        with output_redirected():
             recent = comp.has_function('rados_pool_get_base_tier',
                                        libraries=['rados'])
+
+    print("librados %s 0.94 detected (method rados_pool_get_base_tier %s)" %
+          (">=" if recent else "<=",
+           "detected" if recent else "absent"))
 
     # Generate the source file from template
     cradox_out = os.path.join(os.path.dirname(__file__), 'cradox.pyx')
