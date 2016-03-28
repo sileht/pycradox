d5a510c95d2e9134e9af002aa545cfcb79fca18d
Mehdi Abaakouk <sileht@sileht.net>
2016-03-28 16:27:23 +0200

    Disable ceph osd down on firefly

--- push_in_ceph_tree.sh
+++ push_in_ceph_tree.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-python -c "import setup; setup.pre_build_ext(None, version='latest)"
+python -c "import setup; setup.pre_build_ext(None, version='latest')"
+exit 0
 cp cradox.pyx ../ceph/src/pybind/rados/rados.pyx
 cp cradox.pxd ../ceph/src/pybind/rados/rados.pxd
 sed -e 's/cradox/rados/g' test_rados.py > ../ceph/src/test/pybind/test_rados.py
