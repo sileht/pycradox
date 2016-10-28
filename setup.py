1c7be83a5b687f43127f00824ba270cf0deda563
Mehdi Abaakouk <sileht@sileht.net>
2016-10-28 18:04:13 +0200

    reenable cython 0.25
    cython 0.25.1 now can compile cradox

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def pre_build_ext(cmd_obj, version=None):
 
 if __name__ == '__main__':
     setup(
-        setup_requires=['pbr', 'Cython<0.25', 'Jinja2'],
+        setup_requires=['pbr', 'Cython', 'Jinja2'],
         pbr=True,
         ext_modules=[Extension("cradox", ["cradox.pyx"], libraries=["rados"])],
     )
