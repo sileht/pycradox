1bace50981f3587948c9d85a81227ed1375f6a2e
Mehdi Abaakouk <sileht@sileht.net>
2018-05-29 17:01:35 +0200

    ban cython 0.28.3

--- setup.py
+++ setup.py
@@ -67,7 +67,8 @@ def setup_hook(cmd_obj, version=None):
 if __name__ == '__main__':
     setup(
         # cradox segfault with 0.28.0
-        setup_requires=['pbr', 'Cython!=0.28.0,!=0.28.1,!=0.28.2', 'Jinja2'],
+        setup_requires=['pbr', 'Cython!=0.28.0,!=0.28.1,!=0.28.2,!=0.28.3',
+                        'Jinja2'],
         pbr=True,
         ext_modules=[Extension("cradox", ["cradox.pyx"], libraries=["rados"])],
     )
