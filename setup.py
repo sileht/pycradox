12a8d49c1f38983076ca410a56635fdcfdb17141
Mehdi Abaakouk <sileht@sileht.net>
2018-07-16 11:39:38 +0200

    Avoid cython 0.28.4

--- setup.py
+++ setup.py
@@ -68,7 +68,8 @@ def setup_hook(cmd_obj, version=None):
 if __name__ == '__main__':
     setup(
         # cradox segfault with 0.28.0
-        setup_requires=['pbr', 'Cython!=0.28.0,!=0.28.1,!=0.28.2,!=0.28.3',
+        setup_requires=['pbr',
+                        'Cython!=0.28.0,!=0.28.1,!=0.28.2,!=0.28.3,!=0.28.4',
                         'Jinja2'],
         pbr=True,
         ext_modules=[Extension("cradox", ["cradox.pyx"], libraries=["rados"])],
